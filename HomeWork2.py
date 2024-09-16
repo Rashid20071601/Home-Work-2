77ca9af9cb269d7a36646b16378022c3b09b4f16
Rashid20071601 <153351715+Rashid20071601@users.noreply.github.com>
2024-09-16 07:57:22 +0300

    Add files via upload

---
+++ HomeWork2.py
@@ -0,0 +1,17 @@
+# 1
+the_number_of_completed_DZ = 12
+
+# 2
+number_of_hours_spent = 1.5
+
+# 3
+course_name = 'Python'
+
+# 4
+time_for_one_task = number_of_hours_spent / the_number_of_completed_DZ
+
+# 5
+print(f'Курс:{course_name}, '
+      f'всего задач:{the_number_of_completed_DZ}, '
+      f'затрачено часов:{number_of_hours_spent}, '
+      f'среднее время выполнения {time_for_one_task} часа.')
